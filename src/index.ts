1f2a2ad3c9696d3af0a5956a2421e59c7c948a74
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2026-01-21 11:39:10 -0300

    Tratamento do erro Page Not Found

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { routes } from "./routes/index"
 import { initializeApp } from 'firebase-admin/app';
 import { errorHandler } from "./middlewares/error-handler.middleware"
+import { pageNotFoundHandler } from "./middlewares/page-not-found-handler.middleware";
 
 initializeApp()
 
 const app = express();
 
 routes(app)
+pageNotFoundHandler(app)
 errorHandler(app)
 
 // app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
